d226a03d4550751405b9d92c5d0000ccb5a338a7
Pranay Goel <goel.2002pranay@gmail.com>
2023-06-14 12:24:44 +0530

    fix: notification header z-index

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -11,7 +11,7 @@ function Notifications() {
   console.log('Notifications >>>>>> ',notifications);
 
   return show ? <div className={` overflow-y-auto transition-all border-r border-neutral-200`}>
-    <h2 className="sticky bg-gray-100 top-0 p-4 px-2 page-title w-full border-neutral-200 border-b inline-flex gap-2 items-center "><button onClick={toggleNotificationBar}><IoArrowBack className="text-neutral-800"/></button> Notifications</h2>
+    <h2 className="sticky bg-gray-100 top-0 p-4 z-10 px-2 page-title w-full border-neutral-200 border-b inline-flex gap-2 items-center "><button onClick={toggleNotificationBar}><IoArrowBack className="text-neutral-800"/></button> Notifications</h2>
     {notifications?.length <= 0 ? <Loading /> :  
     <div className="overflow-auto p-4 px-2 flex flex-col gap-2 bg-gray-200 h-full w-full">
       {
