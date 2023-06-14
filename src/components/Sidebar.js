38a0744d45d772f208cd93730b10836e81242209
Pranay Goel <goel.2002pranay@gmail.com>
2023-06-14 12:06:20 +0530

    feat: notification panel + logic + sidebar ui refactoring

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,48 +1,85 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { IoArrowBack, IoLogOut, IoPersonOutline } from 'react-icons/io5'
-import { Link, NavLink } from 'react-router-dom'
-import LogoutLogic from '../Logic/UserLogic.js/Logout.logic'
-import client from '../appwrite.config'
-import { Account } from 'appwrite'
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  IoArrowBack,
+  IoCalendarClearOutline,
+  IoHome,
+  IoHomeOutline,
+  IoLogOut,
+  IoLogOutOutline,
+  IoNotifications,
+  IoNotificationsOutline,
+  IoPersonOutline,
+  IoTicketOutline,
+} from "react-icons/io5";
+import { Link, NavLink } from "react-router-dom";
+import LogoutLogic from "../Logic/UserLogic.js/Logout.logic";
+import client from "../appwrite.config";
+import { Account } from "appwrite";
+import { MdEventBusy } from "react-icons/md";
+import { useNotifications } from "../context/notificationContext";
 
 function Sidebar() {
-  const {logout} = LogoutLogic()
-  const [userInfo, setUserInfo] = useState(null)
+  const { logout } = LogoutLogic();
+  const [userInfo, setUserInfo] = useState(null);
+
+  const { toggleNotificationBar, unreadNotifications } = useNotifications();
 
   const getUserInfo = useCallback(async () => {
     try {
-      const account = new Account(client)
-      const res = await account.get()
-      console.log(res)
-      localStorage.setItem('spotlight-user', JSON.stringify(res))
-      setUserInfo(prev => res)
+      const account = new Account(client);
+      const res = await account.get();
+      console.log(res);
+      localStorage.setItem("spotlight-user", JSON.stringify(res));
+      setUserInfo((prev) => res);
+    } catch (err) {
+      console.error(err);
     }
-    catch(err) {
-      console.error(err)
-    }
-  },[])
+  }, []);
 
   useEffect(() => {
-    getUserInfo()
-  },[getUserInfo])
+    getUserInfo();
+  }, [getUserInfo]);
 
   return (
-    <div className='flex flex-col p-4 border-r border-neutral-200 gap-2'>
-        
-
-        <Link className='sidebar-link' to='/'>
-            Logo
-        </Link>
-        <Link className='sidebar-link' to=''>Home</Link>
-        <NavLink className='sidebar-link' to='events?filter=total'>Events</NavLink>
-        <NavLink className='sidebar-link' to='invities'>Invities</NavLink>
-        <div className='mt-auto flex flex-col'>
-        <NavLink title={userInfo?.email} className='sidebar-link inline-flex items-center gap-1' to='account'>{userInfo ? <><IoPersonOutline/> {userInfo?.name.split(' ')[0]}</> : 'Account'}</NavLink>
-        <button className='sidebar-link' onClick={logout}>Logout</button>
-        </div>
-        
+    <div className="flex flex-col p-4 border-r border-neutral-200 gap-2 ">
+      <Link className="sidebar-link" to="/">
+        Logo
+      </Link>
+      <Link className="sidebar-link" to="">
+        <IoHomeOutline /> Home
+      </Link>
+      <NavLink className="sidebar-link" to="events?filter=total">
+        <IoCalendarClearOutline /> Events
+      </NavLink>
+      <NavLink className="sidebar-link" to="invities">
+        <IoTicketOutline /> Invities
+      </NavLink>
+      <button className="sidebar-link" onClick={toggleNotificationBar}>
+        <div className="relative">
+        <IoNotificationsOutline />
+        {unreadNotifications > 0 && <p className="absolute -top-3 p-2 aspect-square -right-2 bg-primary text-white rounded-full text-[10px] text-center w-2 h-2 flex items-center justify-center"><span className="w-max h-max">{unreadNotifications > 9 ? `9+` : unreadNotifications}</span></p>}
+        </div>Notifications
+      </button>
+      <div className="mt-auto flex flex-col">
+        <NavLink
+          title={userInfo?.email}
+          className="sidebar-link inline-flex items-center gap-1"
+          to="account"
+        >
+          {userInfo ? (
+            <>
+              <IoPersonOutline /> {userInfo?.name?.split(" ")[0]}
+            </>
+          ) : (
+            "Account"
+          )}
+        </NavLink>
+        <button className="sidebar-link" onClick={logout}>
+          <IoLogOutOutline /> Logout
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
---
+++ src/context/notificationContext.js
@@ -0,0 +1,94 @@
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import client from "../appwrite.config";
+import { Databases, ID, Query } from "appwrite";
+import NotificationSound from '../assets/notification.mp3'
+
+const NotificationContext = createContext();
+
+export default function NotificationProvider({ children }) {
+    
+    const [show, setShow] = useState(false);
+    const [notifications, setNotifications] = useState(null);
+    const [unreadNotifications, setUnreadNotifications] = useState(0);
+
+    const toggleNotificationBar = (e) => {
+        e?.preventDefault();
+        setShow(prev => !prev);
+        setUnreadNotifications(prev => 0);
+    }
+
+    const sendNotification = useCallback(async (data) => {
+        try {
+            const db = new Databases(client);
+            const res = await db.createDocument(
+                process.env.REACT_APP_DATABASE_ID,
+                process.env.REACT_APP_NOTIFICATIONS_COLLECTION_ID,
+                ID.unique(),
+                data
+            );
+            console.log(res);
+        } catch (err) {
+            console.error(err);
+        }
+    }, []);
+
+
+    const fetchNotifications = useCallback(async () => {
+        try {
+            const db = new Databases(client);
+            const res = await db.listDocuments(
+                process.env.REACT_APP_DATABASE_ID,
+                process.env.REACT_APP_NOTIFICATIONS_COLLECTION_ID,
+                [
+                    Query.equal('userId', JSON.parse(localStorage.getItem('spotlight-user'))?.$id),
+                    Query.orderDesc('$updatedAt')
+                ]
+            );
+            console.log(res);
+            setNotifications(prev => res.documents);
+        } catch (err) {
+            console.error(err);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchNotifications();
+    }, [fetchNotifications]);
+
+    const uniqueArray = a => [...new Set(a.map(o => JSON.stringify(o)))].map(s => JSON.parse(s))
+
+    console.log('Unread Notifications >>>>>> ', unreadNotifications);
+
+
+    useEffect(() => {
+        console.log('useEffect');
+        client.subscribe(`databases.${process.env.REACT_APP_DATABASE_ID}.collections.${process.env.REACT_APP_NOTIFICATIONS_COLLECTION_ID}.documents`, (res) => {
+            console.log('Payload >>>>>> ', res.payload);
+            if(res.payload?.userId !== JSON.parse(localStorage.getItem('spotlight-user'))?.$id) return;
+            setNotifications(prev => uniqueArray([res.payload, ...prev]));
+            if(!show) {
+                setUnreadNotifications(prev =>  prev + 1);
+            }
+            const audio = new Audio(NotificationSound);
+            audio.play();
+        })
+    })
+
+    const value = {
+        show,
+        toggleNotificationBar,
+        sendNotification,
+        notifications,
+        unreadNotifications
+    }
+
+    return (
+        <NotificationContext.Provider value={value}>
+            {children}
+        </NotificationContext.Provider>
+    )
+}
+
+export function useNotifications() {
+    return useContext(NotificationContext);
+}
